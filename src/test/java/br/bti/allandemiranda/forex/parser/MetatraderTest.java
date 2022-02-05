a372d96a8956bea33ffe1b3f6580d5058073cdc1
Allan de Miranda Silva <allandemiranda@gmail.com>
2022-02-05 14:30:34 +0000

    inova parte

---
+++ src/test/java/br/bti/allandemiranda/forex/parser/MetatraderTest.java
@@ -0,0 +1,63 @@
+package br.bti.allandemiranda.forex.parser;
+
+import br.bti.allandemiranda.forex.model.utils.Candlestick;
+import br.bti.allandemiranda.forex.model.utils.Chart;
+import br.bti.allandemiranda.forex.model.utils.CurrencyExchange;
+import br.bti.allandemiranda.forex.model.utils.CurrencyPair;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.DateTimeException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mockito;
+
+class MetatraderTest {
+
+  @Test
+  void parser() throws URISyntaxException, IOException {
+    // given
+    URL url = MetatraderTest.class.getResource("EURUSD_M15.csv");
+    assert url != null;
+    File file = new File(url.toURI());
+    CurrencyExchange currencyExchange = Mockito.mock(CurrencyExchange.class);
+    // when
+    Mockito.when(currencyExchange.getCurrencyPair()).thenReturn(new CurrencyPair("EUR", "USD"));
+    Chart chart = Metatrader.parser(file, currencyExchange);
+    // then
+    Assertions.assertEquals(3, chart.getCandlestickList().size());
+    Assertions.assertEquals(
+        new Candlestick("1.35442", "1.35502", "1.35437", "1.35502", "1269", "2014-01-23 06:00", currencyExchange.getCurrencyPair()),
+        chart.getCandlestickList().getFirst());
+  }
+
+  @Test
+  void parserTimeThrows() throws URISyntaxException {
+    // given
+    URL url = MetatraderTest.class.getResource("EURUSD_M15_ERRO_TIME.csv");
+    assert url != null;
+    File file = new File(url.toURI());
+    CurrencyExchange currencyExchange = Mockito.mock(CurrencyExchange.class);
+    // when
+    Mockito.when(currencyExchange.getCurrencyPair()).thenReturn(new CurrencyPair("EUR", "USD"));
+    Executable executable = () -> Metatrader.parser(file, currencyExchange);
+    // then
+    Assertions.assertThrows(DateTimeException.class, executable);
+  }
+
+  @Test
+  void parserCandlestickThrows() throws URISyntaxException {
+    // given
+    URL url = MetatraderTest.class.getResource("EURUSD_M15_ERRO_CANDLE.csv");
+    assert url != null;
+    File file = new File(url.toURI());
+    CurrencyExchange currencyExchange = Mockito.mock(CurrencyExchange.class);
+    // when
+    Mockito.when(currencyExchange.getCurrencyPair()).thenReturn(new CurrencyPair("EUR", "USD"));
+    Executable executable = () -> Metatrader.parser(file, currencyExchange);
+    // then
+    Assertions.assertThrows(IllegalArgumentException.class, executable);
+  }
+}
