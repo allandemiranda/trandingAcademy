e3ce2ad3c6b4b2285da9b8bc85cb8fbf4f26fea8
Allan de Miranda Silva <allandemiranda@gmail.com>
2022-02-05 22:22:55 +0000

    deletado

--- target/classes/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.bti.allandemiranda.forex"},{"l":"br.bti.allandemiranda.forex.model.analysis"},{"l":"br.bti.allandemiranda.forex.model.utils"},{"l":"br.bti.allandemiranda.forex.parser"}];updateSearchResults();
